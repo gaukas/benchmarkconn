50232f59c5981603971bc274639a44affcf50e3f
Gaukas Wang <i@gaukas.wang>
2024-06-11 15:40:29 +0800

    new: initial release of core functional code (#1)
    * wip: basic ideas, types, and implementations
    
    Signed-off-by: Gaukas Wang <i@gaukas.wang>
    
    * add: default benchmark types and tests
    
    * Add PressuredBenchmark, a Benchmark implementation sending fixed number of messages of a certain size one after another as quickly as possible.
    
    * Add IntervalBenchmark, a Benchmark implementation sending fixed number of messages of a certain size one after another with a given interval.
    
    * Add test cases for above Benchmark implementations.
    
    Signed-off-by: Gaukas Wang <i@gaukas.wang>
    
    * add: GitHub Actions for Go
    
    Signed-off-by: Gaukas Wang <i@gaukas.wang>
    
    * update: remove unused file
    
    Signed-off-by: Gaukas Wang <i@gaukas.wang>
    
    ---------
    
    Signed-off-by: Gaukas Wang <i@gaukas.wang>

---
+++ benchmark.go
@@ -0,0 +1,421 @@
+package benchmarkconn
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	crand "crypto/rand"
+)
+
+type Benchmark interface {
+	Writer(net.Conn, ...Counter) error
+	Reader(net.Conn, ...Counter) error
+	Result() map[string]any
+}
+
+// PressuredBenchmark is a benchmark that sends a fixed number of messages of a fixed size
+// one after another as fast as possible and measures the throughput and latency.
+type PressuredBenchmark struct {
+	MessageSize   int    `json:"message_size" yaml:"message_size"`     // MessageSize defines how many bytes to write for each send attempt
+	TotalMessages uint64 `json:"total_messages" yaml:"total_messages"` // TotalMessages defines how many messages to send in total
+
+	messageSize      int // an internal copy of the message size used in the last run
+	successfulReads  atomic.Uint64
+	successfulWrites atomic.Uint64
+	startTime        atomic.Value
+	endTime          atomic.Value
+
+	combinedCounter *CombinedCounter
+}
+
+func (b *PressuredBenchmark) Writer(conn net.Conn, counters ...Counter) error {
+	// Compare benchmark specs on both sides
+	specJson, err := json.Marshal(b)
+	if err != nil {
+		return err
+	}
+
+	specLenWr, err := conn.Write(specJson)
+	if err != nil {
+		return err
+	}
+
+	if specLenWr != len(specJson) {
+		return errors.New("failed to write the spec to the connection")
+	}
+
+	receivedSpecJson := make([]byte, 2*len(specJson))
+	specLenRd, err := conn.Read(receivedSpecJson)
+	if err != nil {
+		return err
+	}
+
+	if specLenRd != len(specJson) {
+		return errors.New("failed to read the spec from the connection")
+	}
+
+	if !bytes.Equal(specJson, receivedSpecJson[:specLenRd]) {
+		return errors.New("benchmark specs do not match, aborting")
+	}
+
+	// Create combined counter
+	b.combinedCounter = CombineCounters(time.Second, counters...)
+
+	// Benchmark starts
+	b.messageSize = b.MessageSize
+	b.successfulReads.Store(0)
+	b.successfulWrites.Store(0)
+	b.startTime.Store(time.Now())
+	defer func() {
+		b.endTime.Store(time.Now())
+	}()
+
+	// Start the counter
+	if b.combinedCounter != nil {
+		b.combinedCounter.Start()
+		defer b.combinedCounter.Stop()
+	}
+
+	var randMsg = make([]byte, b.messageSize)
+	var i uint64
+	for i = 0; i < b.TotalMessages; i++ {
+		crand.Read(randMsg)
+		_, err := conn.Write(randMsg)
+		if err != nil {
+			return err
+		}
+		b.successfulWrites.Add(1)
+	}
+
+	return nil
+}
+
+func (b *PressuredBenchmark) Reader(conn net.Conn, counters ...Counter) error {
+	// Compare benchmark specs on both sides
+	specJson, err := json.Marshal(b)
+	if err != nil {
+		return err
+	}
+
+	receivedSpecJson := make([]byte, 2*len(specJson))
+	specLenRd, err := conn.Read(receivedSpecJson)
+	if err != nil {
+		return err
+	}
+
+	if specLenRd != len(specJson) {
+		return errors.New("failed to read the spec from the connection")
+	}
+
+	if !bytes.Equal(specJson, receivedSpecJson[:specLenRd]) {
+		return errors.New("benchmark specs do not match, aborting")
+	}
+
+	specLenWr, err := conn.Write(specJson)
+	if err != nil {
+		return err
+	}
+
+	if specLenWr != len(specJson) {
+		return errors.New("failed to write the spec to the connection")
+	}
+
+	// Create combined counter
+	b.combinedCounter = CombineCounters(time.Second, counters...)
+
+	// Benchmark starts
+	b.messageSize = b.MessageSize
+	b.successfulReads.Store(0)
+	b.successfulWrites.Store(0)
+	b.startTime.Store(time.Now())
+	defer func() {
+		b.endTime.Store(time.Now())
+	}()
+
+	// Start the counter
+	if b.combinedCounter != nil {
+		b.combinedCounter.Start()
+		defer b.combinedCounter.Stop()
+	}
+
+	var receivedMsg = make([]byte, b.messageSize)
+	for b.successfulReads.Load() < b.TotalMessages {
+		_, err := conn.Read(receivedMsg)
+		if err != nil {
+			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
+				return nil
+			}
+			return err
+		}
+		b.successfulReads.Add(1)
+	}
+
+	return nil
+}
+
+func (b *PressuredBenchmark) Result() map[string]any {
+	if b.endTime.Load().(time.Time).IsZero() || b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).Nanoseconds() == 0 {
+		return map[string]any{}
+	}
+
+	result := map[string]any{
+		"successful_reads":  b.successfulReads.Load(),
+		"successful_writes": b.successfulWrites.Load(),
+		"start_time":        b.startTime.Load().(time.Time).Format(time.RFC3339),
+		"end_time":          b.endTime.Load().(time.Time).Format(time.RFC3339),
+		"duration":          b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).String(),
+	}
+
+	// Reader only: calculate ops_per_sec and latency_ms
+	if b.successfulReads.Load() > 0 {
+		result["ops_per_s"] = float64(b.successfulReads.Load()+b.successfulWrites.Load()) / float64(b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).Nanoseconds()) * 1e9
+		result["latency_ns"] = float64(b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).Nanoseconds()) / float64(b.successfulReads.Load()+b.successfulWrites.Load()) // in nanoseconds
+	}
+
+	if b.combinedCounter != nil {
+		result["counters"] = b.combinedCounter.Results()
+	}
+
+	return result
+}
+
+// IntervalBenchmark is a benchmark that sends a fixed number of messages of a fixed size
+// one after another with a fixed interval between each send attempt and measures the
+// throughput and latency.
+type IntervalBenchmark struct {
+	MessageSize   int           `json:"message_size" yaml:"message_size"`     // MessageSize defines how many bytes to write for each send attempt
+	TotalMessages uint64        `json:"total_messages" yaml:"total_messages"` // TotalMessages defines how many messages to send in total
+	Interval      time.Duration `json:"interval" yaml:"interval"`             // Interval defines how long to wait between each send attempt. If this value is too low, it is possible that the actual interval will be much higher due to system limitations
+	Echo          bool          `json:"echo" yaml:"echo"`                     // Echo defines whether the receiver should echo back the received message
+
+	messageSize      int // an internal copy of the message size used in the last run
+	successfulReads  atomic.Uint64
+	successfulWrites atomic.Uint64
+	startTime        atomic.Value
+	endTime          atomic.Value
+
+	echoMap                  *sync.Map     // used for sender to calculate latency
+	totalLatency             atomic.Uint64 // used for sender to calculate latency
+	totalMessagesWithLatency atomic.Uint64 // used for sender to calculate latency
+	ticker                   *time.Ticker
+
+	combinedCounter *CombinedCounter
+}
+
+func (b *IntervalBenchmark) Writer(conn net.Conn, counters ...Counter) error {
+	// Compare benchmark specs on both sides
+	{
+		specJson, err := json.Marshal(b)
+		if err != nil {
+			return err
+		}
+
+		specLenWr, err := conn.Write(specJson)
+		if err != nil {
+			return err
+		}
+
+		if specLenWr != len(specJson) {
+			return errors.New("failed to write the spec to the connection")
+		}
+
+		receivedSpecJson := make([]byte, 2*len(specJson))
+		specLenRd, err := conn.Read(receivedSpecJson)
+		if err != nil {
+			return err
+		}
+
+		if specLenRd != len(specJson) {
+			return errors.New("failed to read the spec from the connection")
+		}
+
+		if !bytes.Equal(specJson, receivedSpecJson[:specLenRd]) {
+			return errors.New("benchmark specs do not match, aborting")
+		}
+	}
+
+	var exitedDueToDeadline atomic.Bool
+
+	// Create combined counter
+	b.combinedCounter = CombineCounters(time.Second, counters...)
+
+	// Benchmark starts
+	b.messageSize = b.MessageSize
+	b.successfulReads.Store(0)
+	b.successfulWrites.Store(0)
+	b.startTime.Store(time.Now())
+	defer func() {
+		if exitedDueToDeadline.Load() {
+			b.endTime.Store(time.Now().Add(-1*time.Second - b.Interval)) // subtract 1 second and the interval to account for the deadline
+		} else {
+			b.endTime.Store(time.Now())
+		}
+	}()
+	b.echoMap = new(sync.Map)
+
+	// Start the counter
+	if b.combinedCounter != nil {
+		b.combinedCounter.Start()
+		defer b.combinedCounter.Stop()
+	}
+
+	var wgEcho sync.WaitGroup
+	if b.Echo { // if echo is enabled start a goroutine to read echoed messages
+		wgEcho.Add(1)
+		go func() {
+			defer wgEcho.Done()
+			var receivedMsg = make([]byte, b.messageSize)
+			for {
+				conn.SetReadDeadline(time.Now().Add(1 * time.Second).Add(b.Interval)) // set a deadline for reading echoed messages
+				n, err := conn.Read(receivedMsg)
+				if err != nil {
+					if errors.Is(err, os.ErrDeadlineExceeded) {
+						exitedDueToDeadline.Store(true)
+					}
+					return
+				}
+				if sendTime, ok := b.echoMap.Load(string(receivedMsg[:n])); ok {
+					b.totalMessagesWithLatency.Add(1)
+					b.echoMap.CompareAndDelete(string(receivedMsg[:n]), sendTime)
+
+					// calculate latency
+					latency := time.Since(sendTime.(time.Time)).Nanoseconds()
+					b.totalLatency.Add(uint64(latency))
+				}
+			}
+		}()
+	}
+
+	// Start sending messages using ticker
+	b.ticker = time.NewTicker(b.Interval)
+
+	var i uint64
+	for i = 0; i < b.TotalMessages; i++ {
+		<-b.ticker.C // wait for the interval
+		var randMsg []byte = make([]byte, b.MessageSize)
+		crand.Read(randMsg)
+
+		if b.Echo { // if echo is enabled, record the message to the echo map
+			sendTime := time.Now()
+			b.echoMap.Store(string(randMsg), sendTime) // save key as hash of the message and value as the time it was sent
+		}
+
+		_, err := conn.Write(randMsg)
+		if err != nil {
+			return err
+		}
+	}
+	b.ticker.Stop()
+
+	wgEcho.Wait()
+
+	return nil
+}
+
+func (b *IntervalBenchmark) Reader(conn net.Conn, counters ...Counter) error {
+	// Compare benchmark specs on both sides
+	{
+		specJson, err := json.Marshal(b)
+		if err != nil {
+			return err
+		}
+
+		receivedSpecJson := make([]byte, 2*len(specJson))
+		specLenRd, err := conn.Read(receivedSpecJson)
+		if err != nil {
+			return err
+		}
+
+		if specLenRd != len(specJson) {
+			return errors.New("failed to read the spec from the connection")
+		}
+
+		if !bytes.Equal(specJson, receivedSpecJson[:specLenRd]) {
+			return errors.New("benchmark specs do not match, aborting")
+		}
+
+		specLenWr, err := conn.Write(specJson)
+		if err != nil {
+			return err
+		}
+
+		if specLenWr != len(specJson) {
+			return errors.New("failed to write the spec to the connection")
+		}
+	}
+
+	// Create combined counter
+	b.combinedCounter = CombineCounters(time.Second, counters...)
+
+	// Benchmark starts
+	b.messageSize = b.MessageSize
+	b.successfulReads.Store(0)
+	b.successfulWrites.Store(0)
+	b.startTime.Store(time.Now())
+	defer func() {
+		b.endTime.Store(time.Now())
+	}()
+
+	// Start the counter
+	if b.combinedCounter != nil {
+		b.combinedCounter.Start()
+		defer b.combinedCounter.Stop()
+	}
+
+	var receivedMsg = make([]byte, b.messageSize)
+	for b.successfulReads.Load() < b.TotalMessages {
+		n, err := conn.Read(receivedMsg)
+		if err != nil {
+			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
+				return nil
+			}
+			return err
+		}
+		b.successfulReads.Add(1)
+
+		if b.Echo { // if echo is enabled, echo back the received message
+			_, err := conn.Write(receivedMsg[:n])
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (b *IntervalBenchmark) Result() map[string]any {
+	if b.endTime.Load().(time.Time).IsZero() || b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).Nanoseconds() == 0 {
+		return map[string]any{}
+	}
+
+	result := map[string]any{
+		"successful_reads":  b.successfulReads.Load(),
+		"successful_writes": b.successfulWrites.Load(),
+		"start_time":        b.startTime.Load().(time.Time).Format(time.RFC3339),
+		"end_time":          b.endTime.Load().(time.Time).Format(time.RFC3339),
+		"duration":          b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).String(),
+	}
+
+	// Reader only: calculate ops_per_sec and latency_ms
+	if b.successfulReads.Load() > 0 {
+		result["ops_per_s"] = float64(b.successfulReads.Load()+b.successfulWrites.Load()) / float64(b.endTime.Load().(time.Time).Sub(b.startTime.Load().(time.Time)).Nanoseconds()) * 1e9
+	}
+
+	if b.totalMessagesWithLatency.Load() > 0 {
+		result["latency_ns"] = float64(b.totalLatency.Load()) / float64(b.totalMessagesWithLatency.Load()) // in nanoseconds
+	}
+
+	if b.combinedCounter != nil {
+		result["counters"] = b.combinedCounter.Results()
+	}
+
+	return result
+}
---
+++ counter.go
@@ -0,0 +1,197 @@
+package benchmarkconn
+
+import (
+	"sync"
+	"time"
+)
+
+type CounterReport interface {
+	Add(time time.Time, value any)
+	Result() map[time.Time]any
+}
+
+type counterReport struct {
+	internalMap *sync.Map
+}
+
+func NewCounterReport() CounterReport {
+	return &counterReport{
+		internalMap: new(sync.Map),
+	}
+}
+
+func (r *counterReport) Add(time time.Time, value any) {
+	r.internalMap.Store(time, value)
+}
+
+func (r *counterReport) Result() (result map[time.Time]any) {
+	result = make(map[time.Time]any)
+	r.internalMap.Range(func(key, value interface{}) bool {
+		result[key.(time.Time)] = value.(int64)
+		return true
+	})
+	return
+}
+
+type Counter interface {
+	CountNow() // CountNow forcibly make the counter take a measurement immediately and save it to the result
+
+	Start()
+	Stop()
+	Result() map[time.Time]any // Result values must be printable and/or JSON-serializable
+}
+
+type CombinedCounter struct {
+	counters []Counter
+
+	ticker   *time.Ticker
+	interval time.Duration
+	closed   chan bool
+}
+
+func CombineCounters(interval time.Duration, counters ...Counter) *CombinedCounter {
+	if len(counters) == 0 {
+		return nil
+	}
+
+	report := make([]CounterReport, len(counters))
+	for i := range report {
+		report[i] = NewCounterReport()
+	}
+	return &CombinedCounter{
+		counters: counters,
+		interval: interval,
+		closed:   make(chan bool),
+	}
+}
+
+func (c *CombinedCounter) CountAllNow() {
+	for _, counter := range c.counters {
+		go counter.CountNow()
+	}
+}
+
+func (c *CombinedCounter) Start() {
+	c.ticker = time.NewTicker(c.interval)
+	go func() {
+		for {
+			select {
+			case <-c.ticker.C:
+				c.CountAllNow()
+			case <-c.closed:
+				return
+			}
+		}
+	}()
+}
+
+func (c *CombinedCounter) Stop() {
+	c.ticker.Stop()
+	close(c.closed)
+}
+
+func (c *CombinedCounter) Results() []map[time.Time]any {
+	results := make([]map[time.Time]any, len(c.counters))
+	for i, counter := range c.counters {
+		results[i] = counter.Result()
+	}
+	return results
+}
+
+// CounterBase is an incomplete implementation of Counter.
+type CounterBase struct {
+	ticker   *time.Ticker
+	interval time.Duration
+	closed   chan bool
+	report   CounterReport
+}
+
+// NewCounterBase creates a new CounterBase and initializes
+// its internal fields.
+func NewCounterBase(interval time.Duration) *CounterBase {
+	return &CounterBase{
+		interval: interval,
+		closed:   make(chan bool),
+		report:   NewCounterReport(),
+	}
+}
+
+// Start starts the ticker only. Implementation
+// of Counter must listen for the ticker and
+// the closed channel.
+func (c *CounterBase) Start() {
+	c.ticker = time.NewTicker(c.interval)
+}
+
+// Stop stops the ticker and closes the closed channel.
+//
+// It is recommended for Counter implementations to directly
+// inherit this method.
+func (c *CounterBase) Stop() {
+	c.ticker.Stop()
+	close(c.closed)
+}
+
+// Result returns the result of the counter.
+//
+// It is recommended for Counter implementations to directly
+// inherit this method.
+func (c *CounterBase) Result() map[time.Time]any {
+	return c.report.Result()
+}
+
+type cpuUsageCounter struct {
+	*CounterBase
+}
+
+func NewCpuUsageCounter(interval time.Duration) Counter {
+	return &cpuUsageCounter{
+		CounterBase: NewCounterBase(interval),
+	}
+}
+
+func (c *cpuUsageCounter) CountNow() {
+	// c.report.Add(time.Now(), 0) // TODO: count CPU usage
+}
+
+func (c *cpuUsageCounter) Start() {
+	c.CounterBase.Start()
+	go func() {
+		for {
+			select {
+			case <-c.ticker.C:
+				c.CountNow()
+			case <-c.closed:
+				return
+			}
+		}
+	}()
+}
+
+type memoryUsageCounter struct {
+	*CounterBase
+}
+
+func NewMemoryUsageCounter(interval time.Duration) Counter {
+	return &memoryUsageCounter{
+		CounterBase: NewCounterBase(interval),
+	}
+}
+
+func (c *memoryUsageCounter) CountNow() {
+	// c.report.Add(time.Now(), 0) // TODO: count memory usage
+}
+
+func (c *memoryUsageCounter) Start() {
+	c.CounterBase.Start()
+	go func() {
+		for {
+			select {
+			case <-c.ticker.C:
+				c.CountNow()
+			case <-c.closed:
+				return
+			}
+		}
+	}()
+}
