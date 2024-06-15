32e9d0724a48fb2e8b5f5b25e93685ee215dfaff
Gaukas Wang <i@gaukas.wang>
2024-06-15 03:06:59 -0600

    feat: client and server as cli tool
    Signed-off-by: Gaukas Wang <i@gaukas.wang>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,170 +1,36 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log/slog"
-	"net"
 	"os"
-	"sync"
-	"time"
 
-	"github.com/gaukas/benchmarkconn"
+	"github.com/gaukas/benchmarkconn/cmd/utils"
 )
-
-const (
-	defaultNetwork = "tcp"
-)
-
-var (
-	network string
-	raddr   string
-
-	messageSize  int
-	totalMessage int
-	interval     time.Duration
-
-	timeout time.Duration
-)
-
-func init() {
-	flag.StringVar(&network, "network", defaultNetwork, "network type (tcp, unix, etc)")
-	flag.StringVar(&network, "net", defaultNetwork, "network type (tcp, unix, etc) (shorthand)")
-	flag.StringVar(&raddr, "raddr", "", "remote address to connect to")
-	flag.StringVar(&raddr, "a", "", "local address to connect to (shorthand)")
-
-	flag.IntVar(&messageSize, "message-size", 1024, "size of the message to send/expect")
-	flag.IntVar(&messageSize, "sz", 1024, "size of the message to send/expect (shorthand)")
-	flag.IntVar(&totalMessage, "total-message", 1000, "total number of messages to send/expect")
-	flag.IntVar(&totalMessage, "m", 1000, "total number of messages to send/expect (shorthand)")
-	flag.DurationVar(&interval, "interval", 1*time.Millisecond, "minimal interval between each message, ignored for type other than echo")
-	flag.DurationVar(&interval, "i", 1*time.Millisecond, "minimal interval between each message, ignored for type other than echo (shorthand)")
-
-	flag.DurationVar(&timeout, "timeout", 10*time.Second, "timeout for the client to wait for the benchmark to finish")
-	flag.DurationVar(&timeout, "t", 10*time.Second, "timeout for the client to wait for the benchmark to finish (shorthand)")
-}
-
-func exitWithUsage() {
-	flag.Usage()
-	fmt.Println("To start the benchmark client: client type command [arguments...]")
-	fmt.Printf("Possible type: pressure, echo\n")
-	fmt.Printf("Possible commands: write, read\n")
-	os.Exit(1)
-}
 
 func main() {
-	flag.Parse()
-	if flag.NArg() != 2 {
-		exitWithUsage()
+	args := os.Args[1:]
+
+	if len(args) < 3 {
+		utils.NewBenchmark().Usage()
+		os.Exit(1)
 	}
 
-	if raddr == "" {
-		fmt.Println("remote address is required")
-		exitWithUsage()
+	b := utils.NewBenchmark()
+
+	benchType := os.Args[1]
+	benchOp := os.Args[2]
+	serverAddr := os.Args[3]
+
+	b.SetBenchType(benchType)
+	b.SetCommand(benchOp)
+	b.SetAddress(serverAddr)
+	if err := b.Init(os.Args[4:]); err != nil {
+		fmt.Printf("Failed to initialize benchmark: %v\n", err)
+		os.Exit(1)
 	}
 
-	benchType := flag.Arg(0)
-	command := flag.Arg(1)
-	var commandWrite bool
-	switch command {
-	case "write":
-		commandWrite = true
-	case "read":
-		commandWrite = false
-	default:
-		fmt.Printf("unknown command: %s\n", command)
-		exitWithUsage()
+	if err := b.Client(); err != nil {
+		fmt.Printf("Failed to run benchmark: %v\n", err)
+		os.Exit(1)
 	}
-
-	switch benchType {
-	case "pressure":
-		pressuredBenchmarkClient(commandWrite)
-	case "echo":
-		echoBenchmarkClient(commandWrite)
-	default:
-		fmt.Printf("unknown type: %s\n", benchType)
-		exitWithUsage()
-	}
-}
-
-func pressuredBenchmarkClient(write bool) {
-	// dial the remote address
-	c, err := net.Dial(network, raddr)
-	if err != nil {
-		slog.Error(fmt.Sprintf("failed to dial %s: %v\n", raddr, err))
-		return
-	}
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		defer c.Close()
-		defer wg.Done()
-
-		pb := &benchmarkconn.PressuredBenchmark{
-			MessageSize:   messageSize,
-			TotalMessages: uint64(totalMessage),
-		}
-
-		if write {
-			pb.Writer(c)
-		} else {
-			pb.Reader(c)
-		}
-
-		slog.Info(fmt.Sprintf("PressuredBenchmark Result: %v", pb.Result()))
-	}()
-
-	go func() {
-		<-time.After(timeout)
-		slog.Warn("timed out, closing the connection")
-		c.Close()
-	}()
-
-	wg.Wait()
-}
-
-func echoBenchmarkClient(write bool) {
-	// dial the remote address
-	c, err := net.Dial(network, raddr)
-	if err != nil {
-		slog.Error(fmt.Sprintf("failed to dial %s: %v\n", raddr, err))
-		return
-	}
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		defer c.Close()
-		defer wg.Done()
-
-		ib := &benchmarkconn.IntervalBenchmark{
-			MessageSize:   messageSize,
-			TotalMessages: uint64(totalMessage),
-			Interval:      interval,
-			Echo:          true,
-		}
-
-		if write {
-			if err := ib.Writer(c); err != nil {
-				slog.Error(fmt.Sprintf("(*IntervalBenchmark).Writer: %v", err))
-				return
-			}
-		} else {
-			if err := ib.Reader(c); err != nil {
-				slog.Error(fmt.Sprintf("(*IntervalBenchmark).Reader: %v", err))
-				return
-			}
-		}
-
-		slog.Info(fmt.Sprintf("EchoBenchmark Result: %v", ib.Result()))
-	}()
-
-	go func() {
-		<-time.After(timeout)
-		slog.Warn("timed out, closing the connection")
-		c.Close()
-	}()
-
-	wg.Wait()
 }
